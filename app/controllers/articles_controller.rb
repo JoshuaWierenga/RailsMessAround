a0dc9952f55bcc630b7e908daf108a1c0d21985c
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2016-10-17 23:31:35 +1100

    FInished creating and showing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,27 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
+    @article = Article.new
   end
  
   def create
     @article = Article.new(article_params)
-
-    @article.save
-    redirect_to @article
-  end
-
-  private
-    def article_params
-      params.require(:article).permit(:title, :text)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
 end
+ 
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
